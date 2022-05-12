dd08befef7e9440d691a8f1c7565db6ed2b8a465
LaPulga1828 <joserodriguez@colegioguanenta.edu.co>
2022-05-12 18:02:34 -0500

    Ejercicio_while

---
+++ ejercicio1/whilesuma.py
@@ -0,0 +1,23 @@
+suma=0
+j=1
+
+print("Dame el valor de N.\n")
+
+# input
+
+n=int(input("."))
+
+#processing
+
+while (j<=n):
+    suma=suma+j
+    j=j+1
+    
+    
+    
+#output
+
+print("La sumaes: " + str (suma))
+
+#FIN
+
